3573179a2f17b01f15af20a9fe51fe475a533bb4
Eric Onasis <ericonasis@gmail.com>
2023-06-24 00:32:12 +0300

    Add a function that checks cases for format handlers

---
+++ _printf_cases.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* case_selector - A struct function that checks the case
+* @format: brings the array's last position before variadic arguments
+* Return: integer value.
+*/
+
+int (*case_selector(const char *format))(va_list list)
+{
+	case_options array[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"d", print_number},
+		{"i", print_number},
+		{NULL, NULL}
+	};
+
+	int position = 0;
+
+	while (array[position].c != NULL)
+	{
+		if (*(array[position].c) == *format)
+		{
+			return (array[position].f);
+		}
+		position++;
+	}
+	return (0);
+}
+
